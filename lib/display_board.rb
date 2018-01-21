56597f20d26c75ebb4570044c03561766f55b095
Mars Wong <marswong618@gmail.com>
2018-01-21 11:53:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  board.each_index do |i|
+    val = board[i] == " " ? "   " : " #{board[i]} "
+
+    if i == 2 || i == 5
+      print "#{val}\n-----------\n"
+    elsif i == 8
+      print "#{val}\n"
+    else
+      print "#{val}|"
+    end
+  end
+end
